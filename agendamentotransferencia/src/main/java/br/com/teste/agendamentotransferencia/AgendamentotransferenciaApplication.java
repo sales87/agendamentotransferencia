80ed0be2683efa43b9086139bcd8b44d2f0e19a4
ffsales <felipe.ferreira.sales@gmail.com>
2017-08-20 13:34:44 -0300

    Primeiro commit

---
+++ agendamentotransferencia/src/main/java/br/com/teste/agendamentotransferencia/AgendamentotransferenciaApplication.java
@@ -0,0 +1,12 @@
+package br.com.teste.agendamentotransferencia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendamentotransferenciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendamentotransferenciaApplication.class, args);
+	}
+}
